3c556a26c523be841358ec58d487b559efec748e
Matias Minoni <99848052+MatiasMinoni@users.noreply.github.com>
2023-02-01 00:57:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -14,15 +14,18 @@ async function test() {
   // Launch sequence
   puppeteer.use(hidden())
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox',],
-    headless: true,
+    ignoreDefaultArgs: ['--enable-automation'],
+    args: ['--no-sandbox', "--disable-blink-features=AutomationControlled"],
+    headless: false,
     ignoreHTTPSErrors: true,
 
     // add this
     executablePath: executablePath(),
   })
-
   const page = await browser.newPage()
+  await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0")
+//   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
+
   await page.setViewport({
     width: 1920,
     height: 1280,
@@ -30,7 +33,11 @@ async function test() {
   });
 
   // Go to page
-  await page.goto('https://www.glami.es/salida/2xu/8124848?o=64&btid=180&t=detail&f=102.301.405.411&til=180', {
+//   await page.goto('https://bot.sannysoft.com/', {
+//     waitUntil: 'networkidle0',
+    
+//   });
+  await page.goto("https://www.glami.es/salida/urban-threads-tall/7361908?o=64&btid=184&t=detail&f=102.301.405.406.411&til=184", {
     waitUntil: 'networkidle0',
     
   });
