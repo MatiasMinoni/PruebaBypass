691ffdaa4c6d82f79878cb181c1601fce6cf3a15
Matias Minoni <99848052+MatiasMinoni@users.noreply.github.com>
2023-02-01 03:14:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const Browser = require('zombie');
 const browser = new Browser();
 
 browser.visit('https://www.glami.es/salida/2xu/8124848?o=64&btid=180&t=detail&f=102.301.405.411&til=180', function() {
+    
     // logear la url actual
     console.log(browser.location.href);
+
+    // logear el html
+    console.log(browser.html());
 
 
     }
