52088ac74ffd2d817c5dcc5357ce4dc58017d9d7
Matias Minoni <99848052+MatiasMinoni@users.noreply.github.com>
2023-02-01 03:02:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-import { create } from 'phantom';
- 
-(async function() {
-  const instance = await create();
-  const page = await instance.createPage();
-  await page.on('onResourceRequested', function(requestData) {
-    console.info('Requesting', requestData.url);
-  });
- 
-  const status = await page.open('https://www.glami.es/salida/2xu/8124848?o=64&btid=180&t=detail&f=102.301.405.411&til=180');
-  const content = await page.property('url');
-  console.log(content);
- 
-  await instance.exit();
-})();
+const Browser = require('zombie');
+const browser = new Browser();
+
+browser.visit('https://www.glami.es/salida/2xu/8124848?o=64&btid=180&t=detail&f=102.301.405.411&til=180', function() {
+    // logear la url actual
+    console.log(browser.location.href);
+
+
+    }
+);
+
